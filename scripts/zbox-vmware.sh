e84cf44bb156324c2e8636c944d5ea4e53a62743
Timo Sugliani <timo.sugliani@gmail.com>
2025-10-24 10:42:39 +0000

    Disable VMware Guest customization through tools

--- scripts/zbox-vmware.sh
+++ scripts/zbox-vmware.sh
@@ -10,6 +10,13 @@ echo '> Installing VMware Software...'
 apt-get install -y \
   open-vm-tools
 
+# Disable VMware guest tools customization of the VM
+# OVF Properties / cloud-init configuration can be used.
+cat >> /etc/vmware-tools/tools.conf << 'EOF'
+[deployPkg]
+enable-customization=false
+EOF
+
 
 # Install govc from github
 wget -qO- https://github.com/vmware/govmomi/releases/download/v0.51.0/govc_Linux_x86_64.tar.gz \
